e368ae75b1b4827120bcb12b91434c328dce3f4f
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-06-27 16:20:10 +0530

    bug

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import SparklesText from "@/components/ui/sparkles-text";
+import { useSession } from "next-auth/react"
+
 const page = () => {
+
+  const session = useSession();
+  const Name = session?.data?.user?.name?.split(" ")[0]
   return (
     <div>
-      Event page
+      Hello {Name} Welcome to <SparklesText text="EventHop"/>
     </div>
   )
 }
